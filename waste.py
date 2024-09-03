1d7435da82a5ba8ef6c6dc47ff39ebc1989fc736
ahmadfouly <143275634+ahmadfouly@users.noreply.github.com>
2024-09-02 21:18:08 -0400

    Update waste.py

--- waste.py
+++ waste.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+import cv2
 
 # Load your model
 model = tf.keras.models.load_model('resnet_model.h5')
@@ -22,22 +23,17 @@ class_names = {
     11: 'white-glass'
 }
 
-def load_image(image):
-    """Resize the image to be suitable for the model."""
-    img = image.resize((224, 224))  # Resize image to 224x224 pixels
-    img_array = np.array(img)  # Convert image to numpy array
-    if img_array.ndim == 2:  # Convert grayscale to RGB if necessary
-        img_array = np.stack((img_array,)*3, axis=-1)
-    if img_array.shape[-1] == 4:  # Handle RGBA images by converting to RGB
-        img_array = img_array[..., :3]
-    img_array = img_array.astype('float32') / 255.0  # Normalize pixel values
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+def load_image(image_file):
+    """Load and resize the image to be suitable for the model."""
+    img = Image.open(image_file)
+    img = img.resize((224, 224))
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)  
     return img_array
 
 def predict(image):
     """Run model prediction on the image and return scores."""
     img_array = load_image(image)
-    st.write(f"Image shape for prediction: {img_array.shape}")  # Debugging output
     predictions = model.predict(img_array)
     scores = tf.nn.softmax(predictions[0]) 
     return scores
@@ -49,7 +45,7 @@ def main():
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
-        scores = predict(image)
+        scores = predict(uploaded_file)
         result_index = np.argmax(scores)
         result_confidence = np.max(scores)
 
